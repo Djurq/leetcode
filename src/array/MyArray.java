8249ba5b4aa7821a67fc6197f0167430d98a7274
Djurq <djurrebax@gmail.com>
2021-11-12 00:41:55 +0100

    plus one works

--- src/array/MyArray.java
+++ src/array/MyArray.java
@@ -92,4 +92,26 @@ public class MyArray {
         }
         return intersect;
     }
+
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9){
+                digits[i] = digits[i] + 1;
+                break;
+            }else{
+                digits[i] = 0;
+                if (i == 0){
+                    digits[i] = 1;
+                    int[] newDigits = new int[digits.length + 1];
+                    digits = newDigits;
+                    digits[0] = 1;
+                    break;
+                }else if (digits[i - 1] != 9){
+                    digits[i - 1] = digits[i - 1] + 1;
+                    break;
+                }
+            }
+        }
+        return digits;
+    }
 }
